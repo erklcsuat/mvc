7d858bcb74c1e0b27ac85d66e5de57aa6830f282
erklcsuat <30703727+erklcsuat@users.noreply.github.com>
2019-09-08 13:13:47 +0300

    Create Route.php

---
+++ Classes/Route.php
@@ -0,0 +1,18 @@
+<?php
+
+class Route
+{
+    public static $validRoutes = array();
+
+    public static function get($route, $function)
+    {
+        self::$validRoutes[] = $route;
+
+        if($_GET['url'] == $route)
+        {
+            $function->__invoke();
+        }
+    }
+}
+
+?>
