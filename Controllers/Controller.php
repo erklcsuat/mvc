4a738ecf1502101d29c878593405c5fcc2d9742a
erklcsuat <30703727+erklcsuat@users.noreply.github.com>
2019-09-08 13:12:53 +0300

    Create Controller.php

---
+++ Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class Controller
+{
+    public static function view($view_name, $data = [])
+    {
+        extract($data);
+        
+        require_once './Views/'.$view_name.'.php';
+    }
+
+    public function model($name)
+    {
+        require_once './Model/'.ucfirst($name). '.php';
+        return new $name();
+    }
+}
